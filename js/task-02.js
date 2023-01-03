12686884d9a76006bfc27e593f2f78b1bb1d5384
Larik7 <Larik777taganka@gmail.com>
2023-01-04 01:50:41 +0200

    first commit

---
+++ js/task-02.js
@@ -0,0 +1,22 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+  const listEl = document.createElement('li');
+  listEl.textContent = ingredient;
+  listEl.classList.add('item');
+
+  return listEl;
+});
+
+console.log(elements);
+
+ingredientsEl.append(...elements);
